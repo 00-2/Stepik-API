1e155a6e5cad1c05774e2f086cf3de8316728cf3
00-2 <9x02@mail.ru>
2023-02-19 14:55:47 +0300

    Add final-score column to table.html

--- examples/external-reports/app.py
+++ examples/external-reports/app.py
@@ -2,33 +2,51 @@ from flask import Flask, request, render_template, session, redirect
 import numpy as np
 import pandas as pd
 import item_report as itr
-DEBUG = True
-
+DEBUG = False
+OUTPUT = True
 app = Flask(__name__,  template_folder='templates')
 contest_table = itr.ContestTable(111634)
-# if DEBUG:
-#     df = []
-#     for i in range(7):
-#         step = pd.read_csv(f'test{i}.csv')
-#         df.append(step)
-#     contest_table._step_submissions = df
+#TODO fix unmeaning nums in saves
+if DEBUG:
+    df = []
+    for i in range(7):
+        step = pd.read_csv(f'test{i}.csv')
+        df.append(step)
+    contest_table._step_submissions = df
 df = contest_table.get_step_submissions()
+if OUTPUT:
+    print(df[0].columns.tolist())
 contesters = contest_table.get_contesters()
-# if DEBUG:
-#     for i, step in enumerate(df):
-#         step.to_csv(f'test{i}.csv', encoding='utf-8')
-df = [ step[['score']].to_dict() for step in df]
+print(contesters)
+if DEBUG:
+    for i, step in enumerate(df):
+        step.to_csv(f'test{i}.csv', encoding='utf-8')
+df = [ step[['score','submission_time']].to_dict() for step in df]
+if OUTPUT:
+    print(df)
 marks = dict()
 for contestant in contesters:
     marks[contestant[0]] = []
 for contestant in contesters:
+    final_score = contestant[1]
     contestant = contestant[0]
     for step in df:
         if 'score' in step:
             if contestant in step['score']:
-                marks[contestant].append(step['score'][contestant])
+                marks[contestant].append(
+                    {
+                        "score":step['score'][contestant],
+                        "submission_time":step['submission_time'][contestant]
+                })
             else:
-                marks[contestant].append(0)
+                marks[contestant].append({
+                        "score":0,
+                        "submission_time":"-"
+                })
+    marks[contestant].append({
+                    "score":final_score,
+                    "submission_time":"-"
+    })
 # for contestant in marks:
 #     print(contestant)
 #     print(marks[contestant])
